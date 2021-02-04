183a0d096419b4f19a735768e03a31bcc1300baf
lmcfadd6 <lmcfadd6@uwo.ca>
2021-02-04 10:50:11 -0500

    constants

--- coordtransform.py
+++ coordtransform.py
@@ -1,4 +1,10 @@
 
+class Constants:
+    def __init__(self):
+
+        self.G = 39.478 #AU^3 yr^-2 M_sun^-1
+
+c = Constants()
 
 class Vector3D:
 
@@ -27,8 +33,19 @@ class Vector3D:
                           self.z * other)
         return result
 
+    def __str__(self):
+        return '[ {:.4f}, {:.4f}, {:.4f}]'.format(self.x, self.y, self.z)
+
     def mag(self):
         result = (self.x**2 + self.y**2 + self.z**2)**0.5
 
-    def __str__(self):
-        return '[ {:.4f}, {:.4f}, {:.4f}]'.format(self.x, self.y, self.z)
+
+def subroutine():
+    pass
+
+if __name__ == "__main__":
+    m_1 = 1
+    m_2 = 1
+    mu = c.G*(m_1 + m_2)
+
+    print(mu)
